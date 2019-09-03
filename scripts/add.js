eb5c6ee6db7f2b5c627333446c220c612dd85923
Octave Raimbault <me@dispix.net>
2019-09-03 10:10:15 +0200

    fix(scripts): fix various issues in the add script

--- scripts/add.js
+++ scripts/add.js
@@ -2,12 +2,30 @@ const fs = require('fs')
 const util = require('util')
 const { exec } = require('child_process')
 
+const readFile = util.promisify(fs.readFile)
 const appendFile = util.promisify(fs.appendFile)
+
+async function validateFile(file) {
+  try {
+    await readFile(file)
+    return null
+  } catch (err) {
+    return err
+  }
+}
 
 ;(async function write() {
   const [, , category, title, url] = process.argv
   const file = `./resources/${category}.md`
   const data = `- [${title}](${url})\n`
+
+  const err = await validateFile(file)
+  if (err) {
+    if (err.code === 'ENOENT') {
+      console.error(`Category ${category} does not exist.`)
+    }
+    return
+  }
 
   appendFile(file, data)
 
