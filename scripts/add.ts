2d7e30ffc77cc1f775effd4dd35c467f97e5bfa7
Octave Raimbault <me@dispix.net>
2020-09-29 14:01:04 +0200

    chore(scripts): add argument check to article command

--- scripts/add.ts
+++ scripts/add.ts
@@ -1,32 +1,26 @@
-const [filePath, title, url] = Deno.args;
+const [filePath, title, url] = Deno.args
 
 if (!filePath || !title || !url) {
-  console.error(`Invalid arguments`);
-  Deno.exit(1);
+  console.error(`./add.sh [file] [title] [url]`)
+  Deno.exit(1)
 }
 
-const category =
-  filePath
-    .split("/")
-    .pop()
-    ?.replace(".md", "") ?? "";
+const category = filePath.split('/').pop()?.replace('.md', '') ?? ''
 
 try {
-  const file = await Deno.open(filePath, { append: true });
+  const file = await Deno.open(filePath, { append: true })
 
-  await file.write(new TextEncoder().encode(`- [${title}](${url})\n`));
-  await Deno.run({ args: ["git", "add", filePath] }).status();
+  await file.write(new TextEncoder().encode(`- [${title}](${url})\n`))
+  await Deno.run({ cmd: ['git', 'add', filePath] }).status()
   await Deno.run({
-    args: ["git", "commit", "-m", `"docs(${category}): add ${title}"`]
-  }).status();
+    cmd: ['git', 'commit', '-m', `"docs(${category}): add ${title}"`],
+  }).status()
 } catch (e) {
   console.error(
-    e.code === "ENOENT" ? `Category ${category} does not exist.` : e
-  );
-  Deno.exit(1);
+    e.code === 'ENOENT' ? `Category ${category} does not exist.` : e,
+  )
+  Deno.exit(1)
 }
 
-console.log("Success!");
-Deno.exit(0);
-
-export {};
+console.log('Success!')
+Deno.exit(0)
